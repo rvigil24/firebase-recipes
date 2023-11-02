5279854f6d92252982c164fe2e355780c1bd874a
rvigil24 <ruben.vigil24@gmail.com>
2023-11-01 20:43:12 -0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import app from './firebase.config'
+import './App.css'
+
+function App() {
+
+  // firebase
+
+  return (
+    <>
+      <div className="App">
+        <div className="title-row">
+          <h1 className='title'>Happy Hacking!</h1>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
